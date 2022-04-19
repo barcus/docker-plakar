00e76a035190f962b5ec3b365136324779decceb
Barcus <barcus@tou.nu>
2022-04-19 17:02:29 +0200

    remove plakar prefix

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,15 +4,9 @@
 INIT=${INIT:=false}
 P_PATH=${P_PATH:=~/.plakar}
 
-if [[ "${INIT}" == "true" ]] || ! [[ -d "${P_PATH}" ]]; then
+if [[ "${INIT}" == "true" ]] ; then
   echo "[!] Init plakar on ${P_PATH}"
   plakar on ${P_PATH} create -no-encryption
 fi
 
-if [ "$1" == "sh" ]; then
-  echo "[!] Launch /bin/sh"
-  exec /bin/sh
-fi
-
-echo "[!] Run plakar on ${P_PATH} $@"
-exec plakar on ${P_PATH} "$@"
+exec "$@"
